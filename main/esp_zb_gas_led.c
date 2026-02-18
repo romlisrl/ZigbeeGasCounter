764995df8540ae5c18e1e99c4b5b98de61eecaaa
Ignacio Hernandez-Ros <ignacio@hernandez-ros.com>
2026-02-18 20:33:32 +0100

    added back missing comment

--- main/esp_zb_gas_led.c
+++ main/esp_zb_gas_led.c
@@ -1,3 +1,14 @@
+/*
+ * Zigbee Gas Meter - An open-source Zigbee gas meter project.
+ * Copyright (c) 2025 Ignacio Hernández-Ros.
+ *
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+ * International License. To view a copy of this license, visit
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/
+ *
+ * You may use, modify, and share this work for personal and non-commercial purposes, as long
+ * as you credit the original author(s) and share any derivatives under the same license.
+ */
 #include "esp_check.h"
 #include "driver/gpio.h"
 
@@ -60,5 +71,3 @@ esp_err_t config_led()
 
 	return ESP_OK;
 }
-
-
